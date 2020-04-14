12de138f54eaa86d3145b00254d354566824cbdd
Micah White <micahjwhite@gmail.com>
2020-04-14 01:21:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  your_hash = {"name" => "Micah White", "location" => "Beacon, New York", "age" => 37}
   
 end
 
