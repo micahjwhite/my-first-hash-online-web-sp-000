37b4b78d6376439d8f2b1dacaacbcfe562619435
Micah White <micahjwhite@gmail.com>
2020-04-14 01:23:04 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   your_hash = {"name" => "Micah White", "location" => "Beacon, New York", "age" => 37}
-  
-end
+  end
 
 
 def shipping_manifest
